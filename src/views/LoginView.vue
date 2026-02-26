0160c93449ec3770e97628e5f00a8fc07d45f63e
davincif <ldavincif@gmail.com>
2026-02-26 08:16:01 +0000

    headlessui installed

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
+import { Switch } from '@headlessui/vue';
 import { ref } from 'vue';
 
 const userNumber = ref('');
 const password = ref('');
 const isLoading = ref(false);
+const enabled = ref(true);
 
 const login = () => {
   console.error('NOT IMPLEMENTED');
@@ -14,37 +16,30 @@ const login = () => {
   <!-- TODO: TAILWIND TESTS. REMOVE IT! -->
   <main>
     <p class="">xablau</p>
-    <button class="bg-sky-500 hover:bg-sky-700 ...">Save changes</button>
-    <br />
-    <button class="bg-sky-500 disabled:hover:bg-sky-500 ...">Save changes</button>
-    <br />
-    <div class="grid grid-cols-2 sm:grid-cols-3">
-      <!-- ... -->
-    </div>
 
-    <div class="bg-white dark:bg-gray-800 rounded-lg px-6 py-8 ring shadow-xl ring-gray-900/5">
-      <div>
+    <Switch
+      v-model="enabled"
+      :class="enabled ? 'bg-blue-600' : 'bg-gray-200'"
+      class="relative inline-flex h-6 w-11 items-center rounded-full"
+    >
+      <span class="sr-only">Enable notifications</span>
+      <span
+        :class="enabled ? 'translate-x-6' : 'translate-x-1'"
+        class="inline-block h-4 w-4 transform rounded-full bg-white transition"
+      />
+    </Switch>
+    <br />
+    <Switch v-model="enabled" as="template" v-slot="{ checked }">
+      <button
+        class="relative inline-flex h-6 w-11 items-center rounded-full"
+        :class="checked ? 'bg-blue-600' : 'bg-gray-200'"
+      >
+        <span class="sr-only">Enable notifications</span>
         <span
-          class="inline-flex items-center justify-center rounded-md bg-indigo-500 p-2 shadow-lg"
-        >
-          <svg
-            class="h-6 w-6 text-white"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-            aria-hidden="true"
-          >
-            <!-- ... -->
-          </svg>
-        </span>
-      </div>
-      <h3 class="text-gray-900 dark:text-white mt-5 text-base font-medium tracking-tight">
-        Writes upside-down
-      </h3>
-      <p class="text-gray-500 dark:text-gray-400 mt-2 text-sm">
-        The Zero Gravity Pen can be used to write in any orientation, including upside-down. It even
-        works in outer space.
-      </p>
-    </div>
+          :class="checked ? 'translate-x-6' : 'translate-x-1'"
+          class="inline-block h-4 w-4 transform rounded-full bg-white transition"
+        />
+      </button>
+    </Switch>
   </main>
 </template>
