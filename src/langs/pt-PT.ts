fd2ee9a83041bdfccb376b983b5000ba1af2e6b2
davincif <ldavincif@gmail.com>
2026-02-26 09:45:44 +0000

    i18n installed

---
+++ src/langs/pt-PT.ts
@@ -0,0 +1,5 @@
+export const pt_PT = {
+  messages: {
+    // code
+  },
+};
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,31 @@
-import './assets/main.css'
+import './assets/main.css';
 
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import { createPinia } from 'pinia';
+import { createApp } from 'vue';
+import { createI18n } from 'vue-i18n';
 
-import App from './App.vue'
-import router from './router'
+import App from './App.vue';
+import { en_US } from './langs/en-US';
+import { es_ES } from './langs/es-ES';
+import { pt_BR } from './langs/pt-BR';
+import { pt_PT } from './langs/pt-PT';
+import router from './router';
 
-const app = createApp(App)
+const i18n = createI18n({
+  legacy: false,
+  locale: 'pt-br',
+  fallbackLocale: 'en-US',
+  messages: {
+    'en-US': en_US.messages,
+    'pt-BR': pt_BR.messages,
+    'pt-PT': pt_PT.messages,
+    'es-ES': es_ES.messages,
+  },
+});
+const app = createApp(App);
 
-app.use(createPinia())
-app.use(router)
+app.use(createPinia());
+app.use(i18n);
+app.use(router);
 
-app.mount('#app')
+app.mount('#app');
