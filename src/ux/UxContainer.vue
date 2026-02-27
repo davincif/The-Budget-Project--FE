64926f63d47874277f3a3122414206881585bbcd
davincif <ldavincif@gmail.com>
2026-02-27 07:54:51 +0000

    UX Container componente done

---
+++ src/ux/UxContainer.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+const { center } = defineProps({
+  center: {
+    type: Boolean,
+    default: false,
+  },
+  _class: {
+    type: String,
+    default: '',
+  },
+});
+</script>
+
+<template>
+  <section class="p-4" :class="[{ 'grid place-items-center': center }, _class]">
+    <slot />
+  </section>
+</template>
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { Switch } from '@headlessui/vue';
+import UxContainer from '@/ux/UxContainer.vue';
 import { ref } from 'vue';
 
 const userNumber = ref('');
@@ -13,33 +13,9 @@ const login = () => {
 </script>
 
 <template>
-  <!-- TODO: TAILWIND TESTS. REMOVE IT! -->
   <main>
-    <p class="">xablau</p>
-
-    <Switch
-      v-model="enabled"
-      :class="enabled ? 'bg-blue-600' : 'bg-gray-200'"
-      class="relative inline-flex h-6 w-11 items-center rounded-full"
-    >
-      <span class="sr-only">Enable notifications</span>
-      <span
-        :class="enabled ? 'translate-x-6' : 'translate-x-1'"
-        class="inline-block h-4 w-4 transform rounded-full bg-white transition"
-      />
-    </Switch>
-    <br />
-    <Switch v-model="enabled" as="template" v-slot="{ checked }">
-      <button
-        class="relative inline-flex h-6 w-11 items-center rounded-full"
-        :class="checked ? 'bg-blue-600' : 'bg-gray-200'"
-      >
-        <span class="sr-only">Enable notifications</span>
-        <span
-          :class="checked ? 'translate-x-6' : 'translate-x-1'"
-          class="inline-block h-4 w-4 transform rounded-full bg-white transition"
-        />
-      </button>
-    </Switch>
+    <UxContainer center>
+      <p>IN CONSTRUCTION</p>
+    </UxContainer>
   </main>
 </template>
